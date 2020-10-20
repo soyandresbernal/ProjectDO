53b077cdea7724e869b09f18302ed895024fc6a6
soyandresbernal <soyandresbernal@gmail.com>
2020-10-20 11:59:57 -0500

    api test  ok

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Módulos requeridos
-const express = require("express");
-const http = require("http");
-const helmet = require("helmet");
-var compression = require("compression");
-require("dotenv").config();
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const http = require('http');
+const helmet = require('helmet');
+var compression = require('compression');
+require('dotenv').config();
+const {v4: uuidv4} = require('uuid');
 
 const app = express();
 app.use(helmet()); // Ayuda a proteger aplicaciones Express
@@ -14,7 +14,17 @@ app.use(compression());
 const serverHttp = http.createServer(app);
 serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
 
-// Prueba
-app.get("/", function (req, res) {
-  res.send("MESSAGE!");
+// Contenido estático
+app.use(express.static('./public'));
+
+// API
+app.get('/api/get-uuid', function(req, res) {
+    res.send(uuidv4())
 });
+
+// 404
+app.get('*', function(req, res) {
+    res.status(404).send('Error 404 - Recurso no encontrado')
+});
+
+
