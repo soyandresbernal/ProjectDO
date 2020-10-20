f1e3c5dbe78f16845ce1e1d8e1ea2f58a66c6555
soyandresbernal <soyandresbernal@gmail.com>
2020-10-20 11:25:58 -0500

    modified .env

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Módulos requeridos
-const express = require('express');
-const http = require('http');
-const helmet = require('helmet');
-var compression = require('compression');
-require('dotenv').config();
-const {v4: uuidv4} = require('uuid');
+const express = require("express");
+const http = require("http");
+const helmet = require("helmet");
+var compression = require("compression");
+require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(helmet()); // Ayuda a proteger aplicaciones Express
@@ -15,6 +15,6 @@ const serverHttp = http.createServer(app);
 serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
 
 // Prueba
-app.get('/', function(req, res) {
-    res.send("Hola mundo")
+app.get("/", function (req, res) {
+  res.send("MESSAGE!");
 });
